d8b02d2f053fa76020b5df6a975ad16b8d0924b7
Suvres <bartosz.blyszcz@gmail.com>
2021-12-02 09:28:15 +0100

    extension load

--- src/DsnetplDoctrineColumnCommentBundle.php
+++ src/DsnetplDoctrineColumnCommentBundle.php
@@ -2,10 +2,18 @@
 
 namespace Dsnetpl\DoctrineColumnCommentBundle;
 
+use Dsnetpl\DoctrineColumnCommentBundle\DependencyInjection\DoctrineColumnCommentBundleExtension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DsnetplDoctrineColumnCommentBundle extends Bundle
 {
+    public function getContainerExtension(): Extension
+    {
+        return new DoctrineColumnCommentBundleExtension();
+    }
+
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
